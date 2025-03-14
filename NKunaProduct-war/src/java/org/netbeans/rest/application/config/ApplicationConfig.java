ad4bfdd50edc7c750a75db80096385621ae29ec7
KIPOKA <ladykipoka@gmail.com>
2025-03-14 23:57:56 +0200

    first commit 3

--- NKunaProduct-war/src/java/org/netbeans/rest/application/config/ApplicationConfig.java
+++ NKunaProduct-war/src/java/org/netbeans/rest/application/config/ApplicationConfig.java
@@ -5,22 +5,17 @@
 package org.netbeans.rest.application.config;
 
 import java.util.Set;
+import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 /**
  *
  * @author ladyk
  */
-@javax.ws.rs.ApplicationPath("webresources")
+@ApplicationPath("webresources")
 public class ApplicationConfig extends Application {
 
-    @Override
-    public Set<Class<?>> getClasses() {
-        Set<Class<?>> resources = new java.util.HashSet<>();
-        addRestResourceClasses(resources);
-        return resources;
-    }
-
+  
     /**
      * Do not modify addRestResourceClasses() method.
      * It is automatically populated with
